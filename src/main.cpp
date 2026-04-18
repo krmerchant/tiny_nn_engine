60c5cab0499fd82298deeac0ea1e2a3c2ff4db6c
Komel Merchant <komelm13@gmail.com>
2026-04-18 16:37:20 -0400

    fix: CPU evaluator to also use batches

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@
 #include <getopt.h>
 #include <iostream>
 
-static void print_usage(const char* prog) {
-  std::cerr << "Usage: " << prog << " -m <model.onnx> -i <images> -l <labels> [-b <batch>]\n"
+static void print_usage(const char *prog) {
+  std::cerr << "Usage: " << prog
+            << " -m <model.onnx> -i <images> -l <labels> [-b <batch>]\n"
             << "\n"
             << "  -m, --model   Path to ONNX model file\n"
             << "  -i, --images  Path to IDX images binary\n"
@@ -17,29 +18,41 @@ static void print_usage(const char* prog) {
 }
 
 int main(int argc, char *argv[]) {
-  const char* model_path  = nullptr;
-  const char* images_path = nullptr;
-  const char* labels_path = nullptr;
-  size_t      batch_size  = 64;
+  const char *model_path = nullptr;
+  const char *images_path = nullptr;
+  const char *labels_path = nullptr;
+  size_t batch_size = 64;
 
   static const option long_opts[] = {
-    {"model",  required_argument, nullptr, 'm'},
-    {"images", required_argument, nullptr, 'i'},
-    {"labels", required_argument, nullptr, 'l'},
-    {"batch",  required_argument, nullptr, 'b'},
-    {"help",   no_argument,       nullptr, 'h'},
-    {nullptr,  0,                 nullptr,  0 }
-  };
+      {"model", required_argument, nullptr, 'm'},
+      {"images", required_argument, nullptr, 'i'},
+      {"labels", required_argument, nullptr, 'l'},
+      {"batch", required_argument, nullptr, 'b'},
+      {"help", no_argument, nullptr, 'h'},
+      {nullptr, 0, nullptr, 0}};
 
   int opt;
-  while ((opt = getopt_long(argc, argv, "m:i:l:b:h", long_opts, nullptr)) != -1) {
+  while ((opt = getopt_long(argc, argv, "m:i:l:b:h", long_opts, nullptr)) !=
+         -1) {
     switch (opt) {
-      case 'm': model_path  = optarg; break;
-      case 'i': images_path = optarg; break;
-      case 'l': labels_path = optarg; break;
-      case 'b': batch_size  = static_cast<size_t>(std::stoul(optarg)); break;
-      case 'h': print_usage(argv[0]); return 0;
-      default:  print_usage(argv[0]); return 1;
+    case 'm':
+      model_path = optarg;
+      break;
+    case 'i':
+      images_path = optarg;
+      break;
+    case 'l':
+      labels_path = optarg;
+      break;
+    case 'b':
+      batch_size = static_cast<size_t>(std::stoul(optarg));
+      break;
+    case 'h':
+      print_usage(argv[0]);
+      return 0;
+    default:
+      print_usage(argv[0]);
+      return 1;
     }
   }
 
@@ -53,8 +66,10 @@ int main(int argc, char *argv[]) {
   model.print_graph();
 
   tinyinfer::MNISTDataset dataset(images_path, labels_path);
-  auto gpu_evaluator = tinyinfer::Evaluator::Builder().batch_size(batch_size).build();
-  auto cpu_evaluator = tinyinfer::Evaluator::Builder().batch_size(1).build();
+  auto gpu_evaluator =
+      tinyinfer::Evaluator::Builder().batch_size(batch_size).build();
+  auto cpu_evaluator =
+      tinyinfer::Evaluator::Builder().batch_size(batch_size).build();
 
   // --- GPU ---
   std::cout << "\n=== GPUExecutor (batch=" << batch_size << ") ===\n";
@@ -72,7 +87,8 @@ int main(int argc, char *argv[]) {
   gpu_exec->print_profile(static_cast<int>(dataset.size() / batch_size));
 
   // --- CPU ---
-  std::cout << "\n=== CPUExecutor (batch=1) ===\n";
+  //
+  std::cout << "\n=== CPUExecutor (batch=" << batch_size << ") ===\n";
   auto cpu_exec = tinyinfer::CPUExecutor::Builder().model(model).build();
   auto t2 = std::chrono::high_resolution_clock::now();
   auto cpu_result = cpu_evaluator.evaluate(*cpu_exec, dataset);
